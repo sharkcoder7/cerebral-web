918f867349447f0a1f7770ddefb151ac699a79ed
deneb0618 <techdeneb@gmail.com>
2019-05-07 18:09:10 -0700

    Added start script

---
+++ server.js
@@ -0,0 +1,17 @@
+var fs = require('fs'),
+    http = require('http');
+
+http.createServer(function (req, res) {
+
+    var url = req.url
+    if (url == '/') url = '/homepage.html'
+  fs.readFile(__dirname + url, function (err,data) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}).listen(8080);
