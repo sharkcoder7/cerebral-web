0e12246b1e00255c9758b727788fbe0c0ca3ae75
deneb0618 <techdeneb@gmail.com>
2019-05-07 19:11:34 -0700

    Use static-server

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
-var fs = require('fs'),
-    http = require('http');
-
-http.createServer(function (req, res) {
-
-    var url = req.url
-    if (url == '/') url = '/homepage.html'
-  fs.readFile(__dirname + url, function (err,data) {
-    if (err) {
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
-}).listen(80);
+var StaticServer = require('static-server');
+var server = new StaticServer({
+  rootPath: '.',            // required, the root of the server file tree
+  port: 80,               // required, the port to listen
+  templates: {
+    index: 'homepage.html',      // optional, defaults to 'index.html'
+  }
+});
+ 
+server.start(function () {
+  console.log('Server listening to', server.port);
+});
+ 
+server.on('request', function (req, res) {
+  // req.path is the URL resource (file name) from server.rootPath
+  // req.elapsedTime returns a string of the request's elapsed time
+});
+ 
+server.on('symbolicLink', function (link, file) {
+  // link is the source of the reference
+  // file is the link reference
+  console.log('File', link, 'is a link to', file);
+});
+ 
+server.on('response', function (req, res, err, file, stat) {
+  // res.status is the response status sent to the client
+  // res.headers are the headers sent
+  // err is any error message thrown
+  // file the file being served (may be null)
+  // stat the stat of the file being served (is null if file is null)
+ 
+  // NOTE: the response has already been sent at this point
+});
